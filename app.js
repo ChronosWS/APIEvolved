0060df6debd6d454f1dea1262dabb46f177ad721
Daniel Gothenborg <daniel@dgw.no>
2015-08-07 12:01:44 +0200

    API Calls are now Cross Origin enabled.

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(bodyParser.json());
 
 app.use('/', router);
 
+// Allow Cross Origin
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 global.state = {
     WaitForUpdate: false
 };
